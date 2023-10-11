e3a036cf6742390d32eeccd0be67178ae7d00df0
koldflames <enoeihoda@gmail.com>
2023-10-11 16:19:45 -0700

    task 7 update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    for keys in sorted(a_dictionary.keys()):
-        print('{}: {}'. format(keys, a_dictionary[keys]))
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
