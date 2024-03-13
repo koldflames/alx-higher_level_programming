9e4e7b16b2e64c82c198ceb93479a747ecd0c699
koldflames <enoeihoda@gmail.com>
2024-03-13 01:55:32 +0000

    renamed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
-#!/usr/bin/node
+#!/usr/bin/node;
+
 const argc = process.argv.length;
 
 if (argc > 2) {
   console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
   console.log('No argument');
-}
+
